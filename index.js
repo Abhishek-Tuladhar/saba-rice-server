5400464cd0fb77f455f3360c22dff07eb7491eb1
abhishek <abhishek@abhisheks-MacBook-Pro.local>
2026-09-01 15:31:54 +0545

    Add Vercel Config

--- index.js
+++ index.js
@@ -26,5 +26,9 @@ app.use("/api/newsletter", newsletterRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+}
+
+module.exports = app;
